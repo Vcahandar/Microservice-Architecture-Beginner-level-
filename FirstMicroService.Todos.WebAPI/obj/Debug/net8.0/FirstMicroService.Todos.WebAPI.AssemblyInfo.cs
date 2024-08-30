e14e48eb3587813f7193a3346d73a8d0f3eb2561
Vcahandar <jahandarmv@code.edu.az>
2024-08-30 20:21:20 +0400

    docker file

--- FirstMicroService.Todos.WebAPI/obj/Debug/net8.0/FirstMicroService.Todos.WebAPI.AssemblyInfo.cs
+++ FirstMicroService.Todos.WebAPI/obj/Debug/net8.0/FirstMicroService.Todos.WebAPI.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("8781f8d0-63d8-43f3-baed-f07734d4a72e")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstMicroService.Todos.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44870ad492f6d64b067f98fd391a63360a8db7f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f6394b39fc277cd3d099cca93c8e04e15c5dfa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstMicroService.Todos.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstMicroService.Todos.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
